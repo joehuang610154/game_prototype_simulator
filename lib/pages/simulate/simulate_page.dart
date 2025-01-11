1e14440677b064372d2c85d2d3c0d093d374a12c
Joe Huang <joehuang610154@gmail.com>
2025-01-11 15:42:37 +0800

    create simulate page

---
+++ lib/pages/simulate/simulate_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SimulatePage extends StatelessWidget {
+  const SimulatePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
