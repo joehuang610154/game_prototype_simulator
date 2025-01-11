5720213249c559b478adda41447dc2a1f31cd114
Joe Huang <joehuang610154@gmail.com>
2025-01-11 15:52:14 +0800

    add button to simulate page

--- lib/pages/simulate/simulate_page.dart
+++ lib/pages/simulate/simulate_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:game_prototype_simulator/constants/widgets.dart';
 
 class SimulatePage extends StatelessWidget {
   const SimulatePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Text("Simulate"),
+            Gaps.w8,
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+              style: IconButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
