ca23348c01371461c18452ebe41eaf79950234ec
Joe Huang <joehuang610154@gmail.com>
2025-01-12 22:41:23 +0800

    extract method

--- test/pages/simulate/simulate_page_test.dart
+++ test/pages/simulate/simulate_page_test.dart
@@ -28,19 +28,44 @@ class SimulatePageTestRunner extends TestRunner {
       testWidgets("load first scene", (tester) async {
         await tester.runAsync(() async {
           var sceneId = "111111";
-          fileSystem.write(
-            SlService.filename,
-            SaveData(scenes: [
-              Scene(id: sceneId, name: "Battle Field"),
-            ]),
-          );
+          givenScenes([
+            Scene(id: sceneId, name: "Battle Field"),
+          ]);
 
           await givenSimulatePage(tester);
 
           shouldShowScene(tester, sceneId);
         });
       });
+
+      testWidgets("rename scene ok", (tester) async {
+        await tester.runAsync(() async {
+          var sceneName = "Battle Field";
+          givenScenes([
+            Scene(id: "222222", name: sceneName),
+          ]);
+
+          await givenSimulatePage(tester);
+
+          expect(find.text(sceneName), findsOneWidget);
+
+          var newSceneName = "New Battle Field";
+
+          await tester.tap(find.byIcon(Icons.edit));
+          await tester.enterText(find.byType(TextFormField), newSceneName);
+          await tester.tap(find.text("Confirm"));
+
+          expect(find.text(newSceneName), findsOneWidget);
+        });
+      }, skip: true);
     });
+  }
+
+  void givenScenes(List<Scene> scenes) {
+    fileSystem.write(
+      SlService.filename,
+      SaveData(scenes: scenes),
+    );
   }
 
   void shouldShowScene(WidgetTester tester, String sceneId) {
