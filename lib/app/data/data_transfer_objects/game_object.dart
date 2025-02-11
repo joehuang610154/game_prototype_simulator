0e842d6db64f82fbc971f42c81dea72f4c836355
Joe Huang <joehuang610154@gmail.com>
2025-02-11 22:13:58 +0800

    remove unused methods

--- lib/app/data/data_transfer_objects/game_object.dart
+++ lib/app/data/data_transfer_objects/game_object.dart
@@ -1,12 +1,7 @@
 import 'package:drift/drift.dart';
 import 'package:game_prototype_simulator/app/data/data_transfer_objects/scene.dart';
 import 'package:game_prototype_simulator/app/data/data_transfer_objects/scene_snapshot.dart';
-import 'package:game_prototype_simulator/app/data/db.dart';
 import 'package:game_prototype_simulator/app/data/entity_table.dart';
-import 'package:game_prototype_simulator/app/domain/entities/game_object.dart';
-import 'package:game_prototype_simulator/app/domain/value_objects/game_object_shape.dart';
-import 'package:game_prototype_simulator/utils/color_extension.dart';
-import 'package:game_prototype_simulator/utils/num_extension.dart';
 
 @DataClassName("GameObjectDto")
 class GameObjectTable extends EntityTable {
@@ -20,35 +15,4 @@ class GameObjectTable extends EntityTable {
   TextColumn get shape => text().withLength(min: 1, max: 10)();
   IntColumn get width => integer()();
   IntColumn get height => integer()();
-}
-
-extension GameObjectDtoExt on GameObjectDto {
-  static fromEntity(GameObject entity) {
-    return GameObjectDto(
-      id: entity.id.toString(),
-      name: entity.name,
-      positionX: entity.position.x.multiply100i(),
-      positionY: entity.position.y.multiply100i(),
-      color: entity.color.toInt(),
-      shape: switch (entity.appearance.shape) {
-        GameObjectShape.circle => 'circle',
-        GameObjectShape.rectangle => 'rectangle',
-      },
-      width: entity.appearance.width.multiply100i(),
-      height: entity.appearance.height.multiply100i(),
-    );
-  }
-
-  GameObject toEntity() {
-    return GameObject.restore(
-      id: id,
-      name: name,
-      positionX: positionX,
-      positionY: positionY,
-      color: color,
-      shape: shape,
-      width: width,
-      height: height,
-    );
-  }
 }
